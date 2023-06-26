6fa0cb65e1a323f9a2889ea7f55404f6c1ead04b
Arnaud Juracek <arnaud.juracek@gmail.com>
2023-06-26 11:38:33 +0200

    Fix issue with Kirby >3.9

--- www/site/config/config.localhost.php
+++ www/site/config/config.localhost.php
@@ -6,11 +6,11 @@ return [
   'cache' => false,
 
   // The code below is required for the kirby-webpack dev server to work
-  'url' => function () {
+  'url' => (function () {
     if (isset($_SERVER['HTTP_X_FORWARDED_FOR']) && $_SERVER['HTTP_X_FORWARDED_FOR'] == 'webpack') {
       return $_SERVER['HTTP_X_FORWARDED_PROTO'] . '://' . $_SERVER['HTTP_X_FORWARDED_HOST'];
     }
 
     return '/';
-  }
+  })()
 ];
