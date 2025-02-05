1badb96b463e21c411c4d762d7766990db115e4d
Arnaud Juracek <arnaud.juracek@gmail.com>
2025-02-05 10:45:01 +0100

    Improve index.js

--- src/index.js
+++ src/index.js
@@ -6,21 +6,36 @@ import lozad from 'lozad'
     await import('/views/default')
   ]
 
-  const components = [
-    await import('/components/Hello')
-  ]
+  const components = {
+    global: [
+      // Outside barba wrapper
+    ],
+    local: [
+      // Inside barba wrapper
+      await import('/components/Hello')
+    ]
+  }
 
   Barba.init({
     debug: true,
     prevent: ({ el }) => el?.classList?.contains('no-barba'),
-    views: views.map(view => view.default)
+    views: views.map(view => view.default),
+    transitions: [{
+      sync: false,
+      once: data => {
+        // Find and hydrate all global components
+        for (const { selector, hydrate } of components.global) {
+          Array.from(document.querySelectorAll(selector)).map(hydrate)
+        }
+      }
+    }]
   })
 
   const refs = new Set()
 
   Barba.hooks.afterEnter(({ next }) => {
     // Hydrate all possible components, keeping a ref
-    for (const { selector, hydrate } of components) {
+    for (const { selector, hydrate } of components.local) {
       for (const el of next.container.querySelectorAll(selector)) {
         refs.add(hydrate(el))
       }
@@ -42,7 +57,7 @@ import lozad from 'lozad'
 
   Barba.hooks.afterLeave(() => {
     // Destroy previous components refs if any
-    for (const ref of refs) ref?.destroy()
+    for (const ref of refs) ref?.destroy?.()
     refs.clear()
   })
 })()
