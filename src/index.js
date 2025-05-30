026a4bae125508a459777082d470d9c20244823b
Arnaud Juracek <arnaud.juracek@gmail.com>
2025-05-30 16:42:41 +0200

    Fix 05a7af0 not throwing errors anymore

--- src/index.js
+++ src/index.js
@@ -22,9 +22,13 @@ import lozad from 'lozad'
     transitions: [{
       sync: false,
       once: data => {
-        // Find and hydrate all global components
-        for (const { selector, hydrate } of components.global) {
-          Array.from(document.querySelectorAll(selector)).map(hydrate)
+        try {
+          // Find and hydrate all global components
+          for (const { selector, hydrate } of components.global) {
+            Array.from(document.querySelectorAll(selector)).map(hydrate)
+          }
+        } catch (e) {
+          console.error(e)
         }
       }
     }]
@@ -33,30 +37,38 @@ import lozad from 'lozad'
   const refs = new Set()
 
   Barba.hooks.afterEnter(({ next }) => {
-    // Hydrate all possible components, keeping a ref
-    for (const { selector, hydrate } of components.local) {
-      for (const el of next.container.querySelectorAll(selector)) {
-        refs.add(hydrate(el))
+    try {
+      // Hydrate all possible components, keeping a ref
+      for (const { selector, hydrate } of components.local) {
+        for (const el of next.container.querySelectorAll(selector)) {
+          refs.add(hydrate(el))
+        }
       }
+
+      // Lazyload
+      const lazys = next.container.querySelectorAll('[data-lazyload]')
+      lozad(lazys).observe()
+      // Lozad set a [data-loaded] attribute when loading image, but does not detect
+      // when image is fully loaded, which can cause incoherent animations
+      for (const image of lazys) image.onload = () => image.setAttribute('data-decoded', true)
+
+      // Reset scroll position
+      window.scrollTo({ top: 0, left: 0, behavior: 'instant' })
+
+      // Flag document as ready
+      document.body.classList.add('is-ready')
+    } catch (e) {
+      console.error(e)
     }
-
-    // Lazyload
-    const lazys = next.container.querySelectorAll('[data-lazyload]')
-    lozad(lazys).observe()
-    // Lozad set a [data-loaded] attribute when loading image, but does not detect
-    // when image is fully loaded, which can cause incoherent animations
-    for (const image of lazys) image.onload = () => image.setAttribute('data-decoded', true)
-
-    // Reset scroll position
-    window.scrollTo({ top: 0, left: 0, behavior: 'instant' })
-
-    // Flag document as ready
-    document.body.classList.add('is-ready')
   })
 
   Barba.hooks.afterLeave(() => {
-    // Destroy previous components refs if any
-    for (const ref of refs) ref?.destroy?.()
-    refs.clear()
+    try {
+      // Destroy previous components refs if any
+      for (const ref of refs) ref?.destroy?.()
+      refs.clear()
+    } catch (e) {
+      console.error(e)
+    }
   })
 })()
